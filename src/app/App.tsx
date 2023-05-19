8cc57f4b061cde08db82374548ad75914c4fc6aa
Vadim Igumnov <vad.igumnoff@gmail.com>
2023-05-19 22:40:31 +0300

    refactor: use feature-sliced design

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,11 +3,12 @@ import './styles/index.scss'
 import { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { useTheme } from './providers/ThemeProvider';
 
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
+
+import { classNames } from 'shared/lib/classNames';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme()
@@ -21,8 +22,8 @@ const App = () => {
       </div>
       <Suspense fallback={<div>Loading</div>}>
         <Routes>
-          <Route path={'/'} element={<MainPageAsync />} />
-          <Route path={'/about'} element={<AboutPageAsync />} />
+          <Route path={'/'} element={<MainPage />} />
+          <Route path={'/about'} element={<AboutPage />} />
         </Routes>
       </Suspense>
     </div>
