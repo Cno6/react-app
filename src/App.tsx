3fbdad9fb0f1f06bd36b9068ad989c39342acfa3
Vadim Igumnov <vad.igumnoff@gmail.com>
2023-05-19 15:28:31 +0300

    feat: light/dark themization

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,21 @@ import { Link, Route, Routes } from "react-router-dom";
 
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Theme, ThemeContext } from './theme/ThemeContext';
+import { useTheme } from './theme/useTheme';
+
+
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme()
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
+      <div>
+        <button onClick={toggleTheme}>Поменять тему</button>
+      </div>
       <Suspense fallback={<div>Loading</div>}>
         <Routes>
           <Route path={'/'} element={<MainPageAsync />} />
