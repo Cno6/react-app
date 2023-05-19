4e8f0d72d6a0cf28ec8536c28a1dceb8b36f9a26
Vadim Igumnov <vad.igumnoff@gmail.com>
2023-05-19 16:51:38 +0300

    feat: helper for add css classes on components

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,15 @@ import { Link, Route, Routes } from "react-router-dom";
 
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { Theme, ThemeContext } from './theme/ThemeContext';
+
 import { useTheme } from './theme/useTheme';
-
-
+import { classNames } from './helpers/classNames';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme()
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {}, [theme])}>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
       <div>
